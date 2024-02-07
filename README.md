7c8392ad635685e74f84d22818d235970df6080b
Dora <cyweng03@gmail.com>
2024-02-06 21:37:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+This is a personal website/portfolio template used for WICC 2024 workshop. It's built with [React](https://reactjs.org).
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Personal Website/Portfolio</h1>
+      <div class="underline">Hello world!</div>
+    </>
+  );
+}
+
+export default App;
